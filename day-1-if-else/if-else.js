ec92b9a3eb367f79e34aae752341aad9d6a25758
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-11-03 22:09:49 +0500

    end

---
+++ day-1-if-else/if-else.js
@@ -0,0 +1,38 @@
+const num = Number(prompt("son kirting"))
+
+// 1-Misol Sharti
+
+// Berilgan son 10 dan katta bo'lsa unga 3 ni qo'shib konsolga chiqaring.
+// Agar 10 dan kichik bo'lsa 2ga ko'paytirib natijani konsolga chiqaring.
+// Agar 10 ga teng bo'lsa 22 sonini konsolga chiqaring.
+
+
+// if (typeof num  === 'number') {
+//     if (num > 10) {
+//         console.log(num+ 3);
+//      }else if(num < 10) {
+//         console.log(num * 2);
+//      }else{
+//         console.log(22);
+//      }
+// }else{
+//     console.log("son kiriting");
+// }
+
+
+
+// 2-Misol Sharti
+
+// Berilgan a sonini 3ga bo'lgandagi butun  qismini b o'zgaruvchiga joylang va b sonini konsolga chiqaring.
+
+
+    // if (num > 2) {
+        
+    //     let num2 = Math.floor( num / 3) 
+    //     console.log(num2);
+    // }else {
+    //     console.log('kiritilgan son 3 dan kichik');
+    // }
+
+
+    
