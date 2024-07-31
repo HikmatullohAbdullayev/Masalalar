af27df1213286564e762a6644ff99ae3f2ffdf9a
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2024-07-31 19:43:07 +0500

    commit

--- masalalar.js
+++ masalalar.js
@@ -29,13 +29,51 @@ const num = Number(prompt("son kirting"))
     // if (num > 2) {
         
     //     let num2 = Math.floor( num / 3) 
-    //     console.log(num2);
-    // }else {
+    //     console.log(num2); 
     //     console.log('kiritilgan son 3 dan kichik');
     // }
 
 
     // Misol-3 Sharti
     
-console.log("shunchakiy");
-    
+ // a soni berilgan. a sonini 3ga bo'lgandagi qoldig'ini kansolga chiqaring.    
+
+ 
+//  if (typeof num  === 'number') 
+//     if (num > 3) {
+//         console.log(num % 3);
+     
+// }else{
+//     console.log("3 dan katta son kiriting");
+// }
+
+// Misol-4 sharti 
+
+// Sonning  juft  yoki  toqligini  aniqlang.  Agar  juft bo'lsa 'juft' so'zini return qiling. Agar toq bo'lsa 'toq' so'zini return qiling.
+
+// console.log(num%2 == 0 ? "Juft son" : "Toq son");
+
+// Misol-5 sharti 
+
+//  a va b sonlari berilgan.  Berilgan ikkita sondan kattasini konsolga chiqaring. Agar ular teng bo'lsa 'Voy ular teng' yozuvi chiqsin.
+
+
+// let son = 5
+
+// if (num === son ){
+//     console.log("Voy ular teng");
+// }else if(num >= son){
+//     console.log(num);   
+// }else{
+//     console.log(son);
+// }
+
+
+// Misol-6 sharti 
+
+// a, b, c  sonlari berilgan.  Shu sonlardan eng kattasini toping. 
+
+let son1 = 4
+let son2 = 10
+
+if()
